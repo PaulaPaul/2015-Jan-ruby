469dfc1fddf58c0e924a3e7d6fa3e026553d177c
PaulaTTS <paula@techtalentsouth.com>
2015-02-08 20:10:34 +0000

    ruby samples for TTS class

---
+++ hashfun.rb
@@ -0,0 +1,9 @@
+my_hash = {"paula@techtalentsouth.com" => "paula", "joe@joeemail.com" => "joe", "jane@mailme.com" => "jane"} 
+puts my_hash
+
+my_hash["martha@me.com"] = "martha"
+puts my_hash
+
+my_hash.delete("joe")
+puts my_hash
+
